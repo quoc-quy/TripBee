779275258231ea3ca70b6209338d2456dce5c736
Binard2112 <mait58674@gmail.com>
2025-11-01 16:47:01 +0700

    handle register by email and password, login by email and password

--- backend/src/main/java/com/tripbee/backend/dto/RegisterRequest.java
+++ backend/src/main/java/com/tripbee/backend/dto/RegisterRequest.java
@@ -10,30 +10,31 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
-    private String name;
     private String email;
-    private String phoneNumber;
-    private String username;
+    private String name;
     private String password;
-
-
-    public String getName() {
-        return name;
-    }
 
     public String getEmail() {
         return email;
     }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    public String getUsername() {
-        return username;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
