50620da19dd8852286371ccd8cf96159077b9eb9
snowyy16 <huynhson532004@gmail.com>
2025-11-10 16:53:13 +0700

    feat: Promotion

--- backend/src/main/java/com/tripbee/backend/repository/PromotionRepository.java
+++ backend/src/main/java/com/tripbee/backend/repository/PromotionRepository.java
@@ -1,4 +1,11 @@
 package com.tripbee.backend.repository;
 
-public interface PromotionRepository {
+import com.tripbee.backend.model.Promotion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PromotionRepository extends JpaRepository<Promotion,Long>,
+        JpaSpecificationExecutor<Promotion> {
 }
