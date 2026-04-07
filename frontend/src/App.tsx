f0887f46ed1c80d4cbc417bf58a9ee1b9619f445
snowyy16 <huynhson532004@gmail.com>
2026-04-07 19:36:41 +0700

    demo4

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,14 @@ import useRouteElements from "./useRouteElements";
 import { ToastContainer } from "react-toastify";
 
 function App() {
-    const routeElements = useRouteElements();
-    return (
-        <div>
-            {routeElements}
-            <ToastContainer />
-        </div>
-    );
+  const routeElements = useRouteElements();
+  return (
+    <div>
+      {routeElements}
+      <ToastContainer />
+      console.log("hello");
+    </div>
+  );
 }
 
 export default App;
