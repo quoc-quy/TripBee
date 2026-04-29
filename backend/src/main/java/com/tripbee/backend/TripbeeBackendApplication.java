51899c72401a8b765d7399a9aa83b8ec448f459e
quoc-quy <quyleom10@gmail.com>
2026-04-29 22:43:21 +0700

    fix: resolve immediate booking expiration on production

--- backend/src/main/java/com/tripbee/backend/TripbeeBackendApplication.java
+++ backend/src/main/java/com/tripbee/backend/TripbeeBackendApplication.java
@@ -1,10 +1,14 @@
 package com.tripbee.backend;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableAsync
@@ -12,6 +16,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableRetry
 public class TripbeeBackendApplication {
 
+	@PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(TripbeeBackendApplication.class, args);
 	}
