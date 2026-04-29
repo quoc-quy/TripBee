257624ad27a470949d89a49dbdfd7ea3456f8d36
quoc-quy <quyleom10@gmail.com>
2026-04-29 19:46:10 +0700

    fix bug: React is declared but its value is never read

--- frontend/src/admin/screens/AdminProtectedRoute/AdminProtectedRoute.tsx
+++ frontend/src/admin/screens/AdminProtectedRoute/AdminProtectedRoute.tsx
@@ -1,13 +1,9 @@
-import { AppContext } from "@/contexts/app.context";
-import React, { useContext } from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { AppContext } from '@/contexts/app.context'
+import { useContext } from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
 
 export default function AdminProtectedRoute() {
-  const { isAuthenticated, profile } = useContext(AppContext);
+  const { isAuthenticated, profile } = useContext(AppContext)
 
-  return isAuthenticated && profile?.role === "ADMIN" ? (
-    <Outlet />
-  ) : (
-    <Navigate to="/admin/login" />
-  );
+  return isAuthenticated && profile?.role === 'ADMIN' ? <Outlet /> : <Navigate to="/admin/login" />
 }
