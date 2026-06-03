e8b6235fc3c3a16947a180c5ced2c521165e969f
quoc-quy <quyleom10@gmail.com>
2026-06-03 14:59:03 +0700

    fix(frontend): restore scroll position to top on route change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import useRouteElements from './useRouteElements'
 import { ToastContainer } from 'react-toastify'
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
   const routeElements = useRouteElements()
   return (
     <div>
+      <ScrollToTop />
       {routeElements}
       <ToastContainer limit={1} />
     </div>
@@ -12,3 +14,4 @@ function App() {
 }
 
 export default App
+
